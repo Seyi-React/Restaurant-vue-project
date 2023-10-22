bc82b3d3bc234f13f95dd522f4b7eadd71af18b4
Seyi-React <oluwaseyiadeoti825@gmail.com>
2023-10-22 19:50:10 +0100

    'sign-up-on-load'

---
+++ vue-project/src/App.vue
@@ -0,0 +1,22 @@
+
+
+<template>
+    <SignUp/>
+     
+</template>
+
+
+<script>
+  import SignUp from './components/SignUp.vue'
+  export default {
+  components:{
+    SignUp
+  }
+
+  }
+
+</script>
+
+<style scoped></style>
+
+
---
+++ vue-project/src/components/SignUp.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="parent">
+  <div>
+    <img src="../assets/new.png" class="sign-logo" />
+    <h2 class="sign">sign up</h2>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style scoped>
+.sign {
+  text-align: center;
+  text-transform: capitalize;
+}
+.sign-logo {
+  width: 150px;
+  height: 150px;
+}
+.parent {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+}
+</style>
