8ba06834f6dc81b84572279360cf032d6e4a2e4b
Seyi-React <oluwaseyiadeoti825@gmail.com>
2023-10-23 04:08:54 +0100

    'mobile-added-signup'

--- vue-project/src/components/SignUp.vue
+++ vue-project/src/components/SignUp.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="parent">
-  <div>
-    <img src="../assets/new.png" class="sign-logo" />
-    <h2 class="sign">sign up</h2>
-  </div>
+    <div>
+      <img src="../assets/new.png" class="sign-logo" />
+      <h2 class="sign">sign up</h2>
+    </div>
+    <div class="register">
+      <input type="text" placeholder="Enter your Name" />
+      <input type="email" placeholder="Enter your Email" />
+      <input type="password" placeholder="Enter your Password" />
+      <button>Sign Up</button>
+    </div>
   </div>
 </template>
 
@@ -17,6 +23,9 @@ export default {
 .sign {
   text-align: center;
   text-transform: capitalize;
+  font-family: system-ui;
+  font-size: 28px;
+  font-weight: 400;
 }
 .sign-logo {
   width: 150px;
@@ -26,6 +35,43 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
+}
+.register {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.register input {
+  display: block;
+  width: 280px;
+  height: 30px;
+  margin: 10px 10px;
+  border: 1px solid gray;
+  outline: none;
+  padding: 5px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.register button {
+  width: 300px;
+  height: 37px;
+  color: white;
+  background-color: black;
+  outline: none;
+  border: none;
+  border-radius: 5px;
+  letter-spacing: 1px;
+  cursor: pointer;
+}
 
+@media (max-width: 550px) {
+  .register input {
+    width: 260px;
+  }
+  .register button {
+    width: 280px;
+  }
 }
 </style>
